4e6ac2ccccc155381fd18b3635a2d15c0fa90c58
quanggiau <lequanggiau123@gmai.com>
2023-03-05 16:55:19 +0700

    add git doc

---
+++ git_doc.sql
@@ -0,0 +1,23 @@
+-- craete: 20230305
+1. git fetch
+-- to update branch hace on remote (origin)
+-- remote: Enumer object count: 100 done
+-- remote: 
+-- remote:
+http:////avo_data.audine_list
+[new branch]    feature/#155 -> origin/feature/#155
+[new branch]    feature/#158 -> origin/feature/#158
+=? git pull to update
+
+2/ git branch -r 
+-- display branch on origin (remote)
+-- have branch not pull
+origin/HEAD ->origin/master --(??)
+origin/develop
+origin/feature/#153
+origin/feature/#154
+origin/feature/#155
+origin/feature/#156
+origin/feature/#157
+origin/feature/#158
+origin/master --(??)
